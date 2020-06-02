52bcf5ad250af8ea532f613ff02ac046c6de259a
rollrat <rollrat.cse@gmail.com>
2020-06-02 10:09:59 +0900

    (hsync) Add base code for hsync

--- tools/hsync/hsync/Program.cs
+++ tools/hsync/hsync/Program.cs
@@ -1,7 +1,10 @@
-﻿// This source code is a part of project violet-server
-// Copyright (C) 2020. rollrat. All rights reserved.
+﻿// This source code is a part of project violet-server.
+// Copyright (C) 2020. rollrat. Licensed under the MIT Licence.
 
+using hsync.Log;
 using System;
+using System.Text;
+using System.Globalization;
 
 namespace hsync
 {
@@ -9,7 +12,57 @@ namespace hsync
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            AppProvider.Initialize();
+
+            Logs.Instance.AddLogNotify((s, e) =>
+            {
+                var tuple = s as Tuple<DateTime, string, bool>;
+                CultureInfo en = new CultureInfo("en-US");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("info: ");
+                Console.ResetColor();
+                Console.WriteLine($"[{tuple.Item1.ToString(en)}] {tuple.Item2}");
+            });
+
+            Logs.Instance.AddLogErrorNotify((s, e) => {
+                var tuple = s as Tuple<DateTime, string, bool>;
+                CultureInfo en = new CultureInfo("en-US");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Error.Write("error: ");
+                Console.ResetColor();
+                Console.Error.WriteLine($"[{tuple.Item1.ToString(en)}] {tuple.Item2}");
+            });
+
+            Logs.Instance.AddLogWarningNotify((s, e) => {
+                var tuple = s as Tuple<DateTime, string, bool>;
+                CultureInfo en = new CultureInfo("en-US");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Error.Write("warning: ");
+                Console.ResetColor();
+                Console.Error.WriteLine($"[{tuple.Item1.ToString(en)}] {tuple.Item2}");
+            });
+
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+            {
+                Logs.Instance.PushError("unhandled: " + (e.ExceptionObject as Exception).ToString());
+            };
+
+            try
+            {
+                Command.Start(args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occured! " + e.Message);
+                Console.WriteLine(e.StackTrace);
+                Console.WriteLine("Please, check log.txt file.");
+            }
+
+            AppProvider.Deinitialize();
+
+            Environment.Exit(0);
         }
     }
 }
